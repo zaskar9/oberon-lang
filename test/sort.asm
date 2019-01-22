67b43f94473c8dc5ff7c69fbed5294237860b794
Michael Grossniklaus <michael.grossniklaus@uni-konstanz.de>
2019-01-22 07:30:46 +0100

    Clean-up of sort.asm

--- test/sort.asm
+++ test/sort.asm
@@ -3,11 +3,13 @@
       extern   printf
 
       section  .data
-val:  db       "%d", 10, 0
-tpl:  db       "(%d,%d)", 10, 0
+num_fmt:
+      db       "%d", 10, 0
+tpl_fmt:
+      db       "(%d,%d)", 10, 0
 
       section  .bss
-arr:  resb     80                ; reserve 20 * 4 bytes
+_a:   resb     80                ; reserve 20 * 4 bytes
 
       section  .text
 ;===============================================================================
@@ -22,16 +24,16 @@ init:
       push     r15
       mov      ebx, 0            ; i := 0
       cmp      ebx, 20           ; i < 20
-      jge      endloop_1
-startloop_1:
-      lea      rcx, [rel arr]    ; @a
+      jge      i_loop_end
+i_loop_start:
+      lea      rcx, [rel _a]     ; @a
       mov      edx, 20
       sub      edx, ebx          ; Dim - i
       mov      [rcx,rbx*4], edx  ; a[i] := Dim - i
       inc      ebx               ; i := i + 1
       cmp      ebx, 20           ; i < 20
-      jl       startloop_1
-endloop_1:
+      jl       i_loop_start
+i_loop_end:
       pop      r15               ; restore callee-save registers
       pop      r14
       pop      r13
@@ -53,17 +55,17 @@ print:
       push     r15
       mov      ebx, 0            ; i := 0
       cmp      ebx, 20           ; i < 20
-      jge      endloop_2
-startloop_2:
-      mov      rdi, val
-      lea      rcx, [rel arr]    ; @a
+      jge      p_loop_end
+p_loop_start:
+      mov      rdi, num_fmt      ; print
+      lea      rcx, [rel _a]     ; @a
       mov      esi, [rcx,rbx*4]  ; a[i]
       xor      rax, rax
       call     printf
       inc      ebx               ; i := i + 1
       cmp      ebx, 20           ; i < 20
-      jl       startloop_2
-endloop_2:
+      jl       p_loop_start
+p_loop_end:
       pop      r15               ; restore callee-save registers
       pop      r14
       pop      r13
@@ -77,6 +79,8 @@ endloop_2:
 swap:
       push     rbp               ; save base pointer
       mov      rbp, rsp          ; set base pointer to stack pointer
+      push     rdi               ; save 1st argument (a)
+      push     rsi               ; save 2nd argumern (b)
       sub      rsp, 8            ; allocate space for one variable and align
       push     rbx               ; save callee-saved registers
       push     r12
@@ -85,9 +89,9 @@ swap:
       push     r15
       mov      ebx, [rdi]        ; perform swap
       mov      r12d, [rsi]
-      mov      [rsi], ebx
       mov      [rdi], r12d
-      mov      rdi, tpl          ; print
+      mov      [rsi], ebx
+      mov      rdi, tpl_fmt      ; print
       mov      esi, ebx
       mov      edx, r12d
       xor      rax, rax
@@ -113,31 +117,31 @@ bubble_sort:
       push     r15
       mov      ebx, 0            ; i := 0
       cmp      ebx, 20           ; i < 20
-      jge      endloop_3
-startloop_3:
+      jge      bs_outer_end
+bs_outer_start:
       mov      ecx, 19           ; j := 19
       cmp      ecx, ebx          ; j > i
-      jle      endloop_4
-startloop_4:
-      lea      rdx, [rel arr]    ; @a
+      jle      bs_inner_end
+bs_inner_start:
+      lea      rdx, [rel _a]     ; @a
       mov      r12d, [rdx-4,rcx*4]
       mov      r13d, [rdx,rcx*4]
       cmp      r12d, r13d        ; a[j - 1] > a[j]
-      jle      endif_1
+      jle      bs_if_end
       lea      rdi, [rdx-4,rcx*4]
       lea      rsi, [rdx,rcx*4]
       mov      [rbp-4], ecx
       call     swap
       mov      ecx, [rbp-4]
-endif_1:
+bs_if_end:
       dec      ecx               ; j := j - 1
       cmp      ecx, ebx          ; j > i
-      jg       startloop_4
-endloop_4:
+      jg       bs_inner_start
+bs_inner_end:
       inc      ebx               ; i := i + 1
       cmp      ebx, 20           ; i < 20
-      jl       startloop_3
-endloop_3:
+      jl       bs_outer_start
+bs_outer_end:
       pop      r15               ; restore callee-save registers
       pop      r14
       pop      r13
@@ -166,9 +170,11 @@ q_sort:
       add      eax, ebx          ; div: set lower 32 bit to r + l
       mov      ecx, 2            ; div: set divisor to 2
       div      ecx               ; (l + r) DIV 2
-      lea      r14, [rel arr]    ; @arr
+      lea      r14, [rel _a]     ; @a
       mov      r13d, [r14,rax*4] ; x := a[(l + r) DIV 2]
-qs_outer:
+      cmp      ebx, r12d
+      jg       qs_outer_end
+qs_outer_start:
       mov      r15d, [r14,rbx*4] ; a[i]
       cmp      r15d, r13d        ; a[i] < x
       jge      qs_scan_ltr_end
@@ -188,29 +194,30 @@ qs_scan_rtl_start:
       jl       qs_scan_rtl_start
 qs_scan_rtl_end:
       cmp      ebx, r12d         ; i <= j
-      jg       qs_if_end
+      jg       qs_if_swap_end
       lea      rdi, [r14,rbx*4]  ; @a[i]
       lea      rsi, [r14,r12*4]  ; @a[j]
       call     swap
       inc      ebx               ; i := i + 1
       dec      r12d              ; j := j - 1
-qs_if_end:
+qs_if_swap_end:
       cmp      ebx, r12d         ; i <= j
-      jle      qs_outer
+      jle      qs_outer_start
+qs_outer_end:
       mov      r15d, [rbp-8]     ; r
       cmp      r15d, r12d        ; r < j
-      jge      qs_if_2_end
+      jge      qs_if_recl_end
       mov      edi, r15d
       mov      esi, r12d
       call     q_sort            ; QSort(r, j)
-qs_if_2_end:
+qs_if_recl_end:
       mov      r15d, [rbp-16]    ; l
       cmp      ebx, r15d         ; i < l
-      jge      qs_if_3_end
+      jge      qs_if_recr_end
       mov      edi, ebx
       mov      esi, r15d
       call     q_sort            ; QSort(i, l)
-qs_if_3_end:
+qs_if_recr_end:
       pop      r15               ; restore callee-save registers
       pop      r14
       pop      r13
@@ -247,21 +254,21 @@ quick_sort:
 main:
       push     rbp               ; save base pointer
       mov      rbp, rsp          ; set base pointer to stack pointer
-      sub      rsp, 8
+      sub      rsp, 8            ; align stack pointer
       push     rbx               ; save callee-saved registers
       push     r12
       push     r13
       push     r14
       push     r15
       call     init
-      lea      rbx, [rel arr]    ; @a
+      lea      rbx, [rel _a]     ; @a
       add      ebx, 28
       mov      edi, ebx
       add      ebx, 36
       mov      esi, ebx
       call     swap              ; Swap(a[7], a[16])
       call     print
-      call     bubble_sort
+      call     quick_sort
       call     print
       pop      r15               ; restore callee-save registers
       pop      r14
