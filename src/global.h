556fc84bffe49066f9fa0b6a87695e5d51d286f0
Michael Grossniklaus <michael.grossniklaus@uni-konstanz.de>
2023-03-20 18:17:14 +0100

    Draft: Compatibility of oberon-lang with GCC
    - support for optimization level, output format and relocation model in `CompilerFlags`
    - adaptation of `Makefile`s to use relocation model when using GCC
    - untangling of namespaces and includes

--- src/global.h
+++ src/global.h
@@ -25,17 +25,5 @@ static std::string to_string(T obj) {
     return stream.str();
 }
 
-enum class OutputFileType {
-    AssemblyFile, BitCodeFile, LLVMIRFile, ObjectFile
-};
-
-enum class OptimizationLevel {
-    O0, O1, O2, O3
-};
-
-enum class RelocationModel {
-    DEFAULT, STATIC, PIC
-};
-
 
 #endif //OBERON_LLVM_GLOBAL_H
