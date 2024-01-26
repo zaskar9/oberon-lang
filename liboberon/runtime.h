3a235d528db5647dff670642bd68ac3f2c92b726
Michael Grossniklaus <michael.grossniklaus@uni-konstanz.de>
2024-01-26 16:03:28 +0100

    Making the Oberon Standard Libray (OSL) more standard compliant. - `Out.Int` and `Out.Long` take two arguments now.

--- liboberon/runtime.h
+++ liboberon/runtime.h
@@ -2,5 +2,11 @@
 // Created by Michael Grossniklaus on 10/21/22.
 //
 
-float realf(int x);
-int entierf(float x);
+#include <time.h>
+
+float rt_realf(int x);
+int rt_entierf(float x);
+
+int rt_timespec_get(struct timespec* const time_spec, int const base);
+
+void rt_out_int(long i, int n);
