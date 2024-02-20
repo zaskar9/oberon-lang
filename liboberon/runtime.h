3751bf97cad57abba86618878d20fee65e7f0f69
Michael Grossniklaus <michael.grossniklaus@uni-konstanz.de>
2024-02-20 11:20:29 +0100

    Draft: Port of Out.Real from Oberon V2

--- liboberon/runtime.h
+++ liboberon/runtime.h
@@ -10,3 +10,10 @@ int rt_entierf(float x);
 int rt_timespec_get(struct timespec* const time_spec, int const base);
 
 void rt_out_int(long i, int n);
+void rt_out_real(float x, int n);
+
+int rt_reals_expo(float x);
+int rt_reals_expoL(double x);
+float rt_reals_ten(int e);
+double rt_reals_tenL(int e);
+void rt_reals_convert(float x, int n, char* d);
