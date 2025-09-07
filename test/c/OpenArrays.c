e54129ead8c95b02c5b245099ba5e73b49aae1e3
Michael Grossniklaus <michael.grossniklaus@uni-konstanz.de>
2025-09-07 21:11:47 +0200

    String comparisons

--- test/c/OpenArrays.c
+++ test/c/OpenArrays.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 void print(int array[], int len) {
   for (int i = 0; i < len; ++i) {
@@ -14,6 +15,12 @@ int main(int argc, char** argv) {
       matrix[10 * i + j] = 10 * i + j;
     }
   }
+  for (int i = 0; i < 10; ++i) {
+    print(&matrix[i * 10], 10);
+  }
+  printf("\n");
+  int array[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+  memcpy(&matrix[10], &array, sizeof array);
   for (int i = 0; i < 10; ++i) {
     print(&matrix[i * 10], 10);
   }
